9ef21da3877dc26795bf01e4e0872eae4221ed78
Nate Martinez <natemartinez@Nates-MacBook-Pro.local>
2021-05-05 21:43:51 -0600

    Major Refactor, Added Context API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import { useContext } from 'react'
 import { Container, Card } from 'react-bootstrap'
 import NavTabs from './components/NavTabs'
 import SearchForm from './components/SearchForm'
-import CardArea from './components/CardArea'
-import { FormContext } from './providers/FormContextProvider'
+import FormContextProvider from './providers/FormContextProvider'
 
 function App() {
-  const { globalContext: { limit } } = useContext(FormContext)
-
-  
-
   return (
     <Container className="App">
-      <Card>
-        <Card.Header>
+      <FormContextProvider>
+        <Card>
           <NavTabs />
-        </Card.Header>
-        <Card.Body>
           <SearchForm />
-        </Card.Body>
-      </Card>
-     {state.values.data.length > 1 ? <CardArea state={state}/> : ''}
+        </Card>
+      </FormContextProvider>
     </Container>
   );
 }
--- src/components/OccupationList.js
+++
@@ -1,8 +0,0 @@
-import { ListGroup } from 'react-bootstrap'
-
-export default function OccupationList({occupations}) {
-  let result = occupations ? '' : occupations.map((job, index) => <ListGroup.Item key={index} variant="dark">{job}</ListGroup.Item>)
-  return (
-    <ListGroup variant="flush">{result}</ListGroup>
-  )
-}
