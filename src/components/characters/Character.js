2f1cf3f37fe93e32f149f56f09bb3384443c3261
Nate Martinez <natemartinez@Nates-iMac.local>
2021-05-10 18:39:48 -0600

    Commit

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,33 +1,56 @@
 import { Card } from 'react-bootstrap'
 import styled from 'styled-components'
 
-export default function Character({character}) {
-  const {name, nickname, img, status} = character
+export default function Character({ character }) {
+  const { 
+    name, 
+    nickname, 
+    img, 
+    status, 
+    occupation, 
+    appearance, 
+    portrayed, 
+    better_call_saul_appearance 
+  } = character
 
+  const OccupationList = () => {
+    let list = occupation.map((job, i) => (<li key={ i }>{ job }</li>))
+    return (
+      <ul>{ list }</ul>
+    )
+  }
+  const SeasonsList = () => {
+    let list = appearance.map((season, i) => (<li key={ i }>Season: { season }</li>))
+    return (
+      <ul>{ list }</ul>
+    )
+  }
+  
   return (
   <CharacterCard>
-    <CharacterImg variant="top" src={img} fluid/>
-    <Card.ImgOverlay className='text-white'>
-      <Card.Text>{ status }</Card.Text>
-    </Card.ImgOverlay>
+    <CharacterImg variant="top" src={ img } fluid/>
     <CardBody>
-      <Card.Title>{ name }</Card.Title>
+      <Card.Title>{ name }<small>Actor: { portrayed }</small></Card.Title>
       <Card.Text>{ nickname }</Card.Text>
+      Occupations: 
+      <OccupationList />
+      Breaking Bad:
+      <SeasonsList />
     </CardBody>
   </CharacterCard>
   )
 }
 
 const CharacterCard = styled(Card)`
-box-shadow: 0 0 8px 1px rgba(0,0,0,0.1);
-&:hover {
-  cursor: pointer;
-  box-shadow: 0 0 8px 1px rgba(0,0,0,0.3);
-}
+  box-shadow: 0 0 8px 1px rgba(0,0,0,0.1);
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 0 8px 1px rgba(0,0,0,0.3);
+  }
 `
 const CharacterImg = styled(Card.Img)`
-height: 25rem;
+  height: 25rem;
 `
 const CardBody = styled(Card.Body)`
-text-align: center;
+ 
 `
